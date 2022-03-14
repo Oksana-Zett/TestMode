5114fd7e749c6836e846abf6c491811c801eded8
Oksana Zateryukina <zateryukinaoksana@mail.ru>
2022-03-14 23:33:13 +0300

    Updated

--- src/test/java/test/AuthTest.java
+++ src/test/java/test/AuthTest.java
@@ -16,7 +16,7 @@ public class AuthTest {
 
     @BeforeEach
     void setup() {
-        //Configuration.headless = true;
+        Configuration.headless = true;
         open("http://localhost:9999");
     }
 
